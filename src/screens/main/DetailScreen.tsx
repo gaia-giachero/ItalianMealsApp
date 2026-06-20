945634d380b70c58a3f010554c8e40df871cf318
Gaia Giachero <gaia.giachero@edu-its.it>
2026-06-20 17:34:49 +0200

    feat(DetailScreen): add at page image, name, ingredients and instructions

--- src/screens/main/DetailScreen.tsx
+++ src/screens/main/DetailScreen.tsx
@@ -1,8 +1,46 @@
-import { Text, Pressable, View, StyleSheet } from "react-native";
+import React from "react";
+import {
+  Text,
+  Pressable,
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  Image,
+  ScrollView,
+} from "react-native";
+import { fetchMealById } from "../../services/meals";
+import { Ionicons } from "@expo/vector-icons";
+
+interface DetailsMeals {
+  strMeal: string;
+  strMealThumb: string;
+  strInstructions: string;
+  ingredients: string[];
+}
 
 export default function DetailsScreen({ route, navigation }: any) {
   const id = route.params?.id;
-  const path = id ? `myapp://dettagli/${id}` : undefined;
+
+  const [meal, setMeal] = React.useState<DetailsMeals | undefined>(undefined);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState<string | undefined>(undefined);
+
+  async function mealById() {
+    setError(undefined);
+    setLoading(true);
+    try {
+      const detailsItem = await fetchMealById(id);
+      setMeal(detailsItem);
+    } catch (errore) {
+      setError("Riprova ricaricando la pagina");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  React.useEffect(() => {
+    mealById();
+  }, []);
 
   if (!id) {
     return (
@@ -16,17 +54,30 @@ export default function DetailsScreen({ route, navigation }: any) {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.center}>
-        <Text style={styles.idText}>ID: {id}</Text>
-        <Text>Path: {path}</Text>
-
-      </View>
-
-      <Pressable style={styles.button} onPress={() => navigation.goBack()}>
-        <Text style={styles.buttonText}>Torna alla Home</Text>
-      </Pressable>
-    </View>
+    <ScrollView>
+      {loading ? (
+        <View>
+          <ActivityIndicator size="small" color="#000" />
+          <Text>Caricamento del piatto in corso...</Text>
+        </View>
+      ) : error ? (
+        <View>
+          <Pressable onPress={mealById}>
+            <Ionicons name="refresh" size={24} color="black" />
+          </Pressable>
+          <Text>Riprova ricaricando la pagina</Text>
+        </View>
+      ) : (
+        <View>
+          <Image source={{ uri: meal?.strMealThumb }} style={styles.image} />
+          <Text>{meal?.strMeal}</Text>
+          {meal?.ingredients.map((i, index) => 
+            <Text key={index.toString()}>{i}</Text>
+          )}
+          <Text>{meal?.strInstructions}</Text>
+        </View>
+      )}
+    </ScrollView>
   );
 }
 
@@ -59,5 +110,9 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "red",
     marginBottom: 20,
+  },
+  image: {
+    width: 300,
+    height: 200,
   },
 });
