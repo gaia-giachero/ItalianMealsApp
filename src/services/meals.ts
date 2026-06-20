d57ed6df1e3546af741454a61b50ec0a604e34a5
Gaia Giachero <gaia.giachero@edu-its.it>
2026-06-20 17:36:38 +0200

    refactor(meals): add method for create and return a filtered array with ingredients

--- src/services/meals.ts
+++ src/services/meals.ts
@@ -11,5 +11,18 @@ export async function fetchMealById(id: string) {
   const res = await fetch(`${BASE}/lookup.php?i=${id}`);
   if (!res.ok) throw new Error(`HTTP ${res.status}`);
   const data = await res.json();
-  return data.meals?.[0] ?? null;
+  const meal = data.meals?.[0];
+  const ingredients = Array.from(
+    { length: 20 },
+    (_, i) => meal[`strIngredient${i + 1}`],
+  ).filter(
+    (ingredient) =>
+      ingredient != null && ingredient != undefined && ingredient != "",
+  );
+  return {
+    strMeal: meal.strMeal,
+    strMealThumb: meal.strMealThumb,
+    strInstructions: meal.strInstructions,
+    ingredients,
+  };
 }
