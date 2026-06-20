3d89303a7e843dd86713483af74bb6f6a23deafa
Gaia Giachero <gaia.giachero@edu-its.it>
2026-06-20 10:55:44 +0200

    refarctor(App): export function in AppNavigator file

--- App.tsx
+++ App.tsx
@@ -1,36 +1,12 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-
-import LoginScreen from "./src/screens/auth/LoginScreen";
-import HomeScreen from "./src/screens/main/HomeScreen";
-import DetailsScreen from "./src/screens/main/DetailScreen";
-
-const Stack = createNativeStackNavigator();
-
-const linking = {
-  prefixes: ["myapp://"],
-  config: {
-    screens: {
-      Login: "login",
-      Home: "home",
-      Details: "dettagli/:id",
-    },
-  },
-};
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        <NavigationContainer linking={linking}>
-          <Stack.Navigator>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Details" component={DetailsScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <AppNavigator />
       </SafeAreaView>
     </SafeAreaProvider>
   );
