4f86e5d4998efd96f743fd633775ba9cf38e99e0
Gaia Giachero <gaia.giachero@edu-its.it>
2026-06-22 23:30:56 +0200

    feat(TabNavigator): add icon home and setting

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,17 +1,37 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import HomeScreen from "../screens/main/HomeScreen";
-import DetailsScreen from "../screens/main/DetailScreen";
 import SettingScreen from "../screens/main/SettingsScreen";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Setting" component={SettingScreen} />
-        </Tab.Navigator>
-    )
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false, // Nasconde la scritta testuale sotto le icone
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Setting"
+        component={SettingScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
