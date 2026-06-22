6dd62c625b66ca6ea96d5e2444c30eecba3dbd0e
Gaia Giachero <gaia.giachero@edu-its.it>
2026-06-22 23:32:49 +0200

    feat(SettingsScreen): add icon and change temporarely ui of page

--- src/screens/main/SettingsScreen.tsx
+++ src/screens/main/SettingsScreen.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+
+// import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function SettingScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Schermata Impostazioni (Work in Progress)</Text>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <MaterialIcons name="engineering" size={48} color="orange" />
+      <Text>Quando ho voglia la farò... per ora ti becchi questo :)</Text>
+    </View>
+  );
 }
