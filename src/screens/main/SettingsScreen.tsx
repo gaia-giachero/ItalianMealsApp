efc31fbe5f19a9a260d7dafc93a8ebfe6e76e1b5
Gaia Giachero <gaia.giachero@edu-its.it>
2026-06-25 00:10:50 +0200

    style(SettingsScreen): add style, WIP

--- src/screens/main/SettingsScreen.tsx
+++ src/screens/main/SettingsScreen.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import { View, Text } from "react-native";
 
 // import { Ionicons } from "@expo/vector-icons";
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from "@expo/vector-icons";
+import { globalStyles } from "../../theme/style";
+import { colors } from "../../theme/colors";
 
 export default function SettingScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <MaterialIcons name="engineering" size={48} color="orange" />
-      <Text>Quando ho voglia la farò... per ora ti becchi questo :)</Text>
+    <View style={[globalStyles.container, globalStyles.centered]}>
+      <MaterialIcons name="engineering" size={48} color={colors.warning} />
+      <Text
+        style={[
+          globalStyles.text,
+          { marginTop: 12, textAlign: "center", paddingHorizontal: 30 },
+        ]}
+      >
+        Quando ho voglia la farò... per ora ti becchi questo :)
+      </Text>
     </View>
   );
 }
