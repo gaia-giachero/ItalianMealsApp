fdb59f341e0f1803aa9339603e16a083ad552a4b
Gaia Giachero <gaia.giachero@edu-its.it>
2026-06-25 00:02:36 +0200

    style(App): import external file with style

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { globalStyles } from "./src/theme/style";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={globalStyles.container}>
         <AppNavigator />
       </SafeAreaView>
     </SafeAreaProvider>
