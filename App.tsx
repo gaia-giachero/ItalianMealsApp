807e91497b228660f0c1b463c223899e013c1919
Gaia Giachero <gaia.giachero@edu-its.it>
2026-06-26 23:38:45 +0200

    feat(App): add AuthProvider and FavoritesProvider

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+import AuthProvider from "./src/context/AuthContext";
+import FavoritesProvider from "./src/context/FavoritesContext";
+
 import AppNavigator from "./src/navigation/AppNavigator";
+
 import { globalStyles } from "./src/theme/style";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={globalStyles.container}>
-        <AppNavigator />
+        <AuthProvider>
+          <FavoritesProvider>
+            <AppNavigator />
+          </FavoritesProvider>
+        </AuthProvider>
       </SafeAreaView>
     </SafeAreaProvider>
   );
