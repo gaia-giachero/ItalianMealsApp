1e60c79fbb3ef2471aab709883e52dd74132caba
Gaia Giachero <gaia.giachero@edu-its.it>
2026-07-02 12:52:04 +0200

    feat(HeaderProfile): add navigation to setting on press on the avatar or name

--- src/components/HeaderProfile.tsx
+++ src/components/HeaderProfile.tsx
@@ -4,18 +4,22 @@ import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { colors } from "../theme/colors";
 import { globalStyles } from "../theme/style";
 import { AuthContext } from "../context/AuthContext";
+import { useNavigation } from '@react-navigation/native';
 
 export default function HeaderProfile() {
     const { name, avatarUri, logout } = useContext(AuthContext);
+    const navigation = useNavigation<any>();
 
     return (
-        < View style={globalStyles.header} >
-            <View style={styles.avatarName}>
+        <View style={globalStyles.header} >
+            <Pressable style={styles.avatarName} onPress={() =>
+                navigation.navigate("Setting")
+            }>
                 <View style={styles.avatarContainer}>
                     <Image source={{ uri: avatarUri }} style={styles.avatar} />
                 </View>
                 <Text style={styles.nomeCognome}>{name}</Text>
-            </View>
+            </Pressable>
             <Pressable onPress={logout}>
                 <Ionicons name="log-out-outline" size={25} color="black" />
             </Pressable>
@@ -24,26 +28,26 @@ export default function HeaderProfile() {
 }
 
 const styles = StyleSheet.create({
-  avatarName: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 10,
-  },
-  nomeCognome: {
-    fontSize: 18,
-    fontWeight: "700",
-    letterSpacing: 0.9,
-    color: "#000",
-  },
-  avatarContainer: {
-    width: 52,
-    height: 52,
-    borderRadius: 26,
-    overflow: "hidden",
-    backgroundColor: colors.placeholder,
-  },
-  avatar: {
-    width: 52,
-    height: 52,
-  },
+    avatarName: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+    },
+    nomeCognome: {
+        fontSize: 18,
+        fontWeight: "700",
+        letterSpacing: 0.9,
+        color: "#000",
+    },
+    avatarContainer: {
+        width: 52,
+        height: 52,
+        borderRadius: 26,
+        overflow: "hidden",
+        backgroundColor: colors.placeholder,
+    },
+    avatar: {
+        width: 52,
+        height: 52,
+    },
 });
